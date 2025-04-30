96244c0af015fb398b659cb2dc0e0db8448a0629
Kian Naderi <kian.naderi1002@gmail.com>
2025-04-30 15:08:50 -0700

    Added the printGameRules function and included our names at the top of the file in the comment, per the submission rules for the check in

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
+// Group Members: Kian Naderi, David Carbajal, Charlton Le, Danny Tran, Lucas Taylor
+
 #include <stdio.h>
 #include <stdlib.h>
 
-//Creating block structure
+// Creating block structure
 #define block_size 4
+
 struct Block {
     char shape[block_size][block_size];
     int width;
     int height;
 };
 
+void printGameRules();
 void initializeGameBoard(char gameBoard[20][10]);
 void displayGameBoard(char gameBoard[20][10]);
 void displayBlock(struct Block b);
@@ -75,6 +79,8 @@ struct Block blocks[7] = {
 int main() {
     printf("Hello Player! Welcome to Tetris!\n");
 
+    printGameRules();
+
     // Declare and define the game board
     char gameBoard[20][10] = {0};
 
@@ -85,11 +91,17 @@ int main() {
     printf("\n Example I block:\n");
     displayBlock(blocks[0]);
 
-
-    // Placeholder for game loop
-    // Game loop would go here.
-
+    // Game loop goes here.
+    
     return 0;
+}
+
+void printGameRules() {
+    printf("Here are the rules of the game:\n");
+    printf("Rule #1. Use the arrow keys on your device to move the blocks.\n");
+    printf("Rule #2. Press 'q' to quit the game at any time.\n");
+    printf("Rule #3. Once a line has been filled in with tetris blocks, that line will be cleared from the board.\n");
+    printf("Rule #4: The game ends when any block reaches the top of the board.\n\n");
 }
 
 void initializeGameBoard(char gameBoard[20][10]) {
