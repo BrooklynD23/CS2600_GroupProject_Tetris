c37e9514fe21f988504ee4e1e54f399a5bd369ba
Kian Naderi <kian.naderi1002@gmail.com>
2025-04-30 14:58:03 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,76 +1,11 @@
+// Group Members: Kian Naderi, David Carbajal, Charlton Le, Danny Tran, and Lucas Taylor
+
 #include <stdio.h>
 #include <stdlib.h>
 
-//Creating block structure
-#define block_size 4
-struct Block {
-    char shape[block_size][block_size];
-    int width;
-    int height;
-};
-
+// Function prototypes
 void initializeGameBoard(char gameBoard[20][10]);
 void displayGameBoard(char gameBoard[20][10]);
-void displayBlock(struct Block b);
-
-struct Block blocks[7] = {
-    // Horizontal I
-    {{
-        {'X', 'X', 'X', 'X'},
-        {' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' '}
-    }, 4, 1},
-
-    // T
-    {{
-        {'X', 'X', 'X', ' '},
-        {' ', 'X', ' ', ' '},
-        {' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' '}
-    }, 3, 2},
-
-    // O
-    {{
-        {'X', 'X', ' ', ' '},
-        {'X', 'X', ' ', ' '},
-        {' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' '}
-    }, 2, 2},
-
-    // S
-    {{
-        {' ', 'X', 'X', ' '},
-        {'X', 'X', ' ', ' '},
-        {' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' '}
-    }, 3, 2},
-
-    // L
-    {{
-        {'X', ' ', ' ', ' '},
-        {'X', 'X', 'X', ' '},
-        {' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' '}
-    }, 3, 2},
-
-    // J
-    {{
-        {' ', ' ', 'X', ' '},
-        {'X', 'X', 'X', ' '},
-        {' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' '}
-    }, 3, 2},
-
-    // Z
-    {{
-        {'X', 'X', ' ', ' '},
-        {' ', 'X', 'X', ' '},
-        {' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' '}
-    }, 3, 2}
-};
-
 
 int main() {
     printf("Hello Player! Welcome to Tetris!\n");
@@ -82,17 +17,14 @@ int main() {
     initializeGameBoard(gameBoard);
     displayGameBoard(gameBoard);
 
-    printf("\n Example I block:\n");
-    displayBlock(blocks[0]);
-
-
     // Placeholder for game loop
     // Game loop would go here.
+    
 
     return 0;
 }
 
-void initializeGameBoard(char gameBoard[20][10]) {
+void initializeBoard(char gameBoard[20][10]) {
     for (int i = 0; i < 20; i++) {
         for (int j = 0; j < 10; j++) {
             gameBoard[i][j] = '.';
@@ -104,19 +36,6 @@ void displayGameBoard(char gameBoard[20][10]) {
     for (int i = 0; i < 20; i++) {
         for (int j = 0; j < 10; j++) {
             printf("%c ", gameBoard[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void displayBlock(struct Block b) {
-    for (int i = 0; i < b.height; i++) {
-        for (int j = 0; j < b.width; j++) {
-            if (b.shape[i][j] == 'X') {
-                printf("X ");
-            } else {
-                printf(". ");
-            }
         }
         printf("\n");
     }
